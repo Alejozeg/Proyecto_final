f9dec2f7c757924066073da0169b8925753f272d
Alejozeg <pipezeas@gmail.com>
2024-08-05 17:33:32 -0500

    1.0

---
+++ script.js
@@ -0,0 +1,8 @@
+function ModoOscuro(){
+    const color= document.body.style.backgroundColor
+    if (color==="f3e5d8" || color===""){
+        document.body.style.backgroundColor="black"
+    }else{
+        document.body.style.backgroundColor="#f3e5d8"
+    } 
+} 
