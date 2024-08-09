2c61fb1f578ef91bc6927af9d19b8001fa76f573
Alejozeg <pipezeas@gmail.com>
2024-08-09 17:51:39 -0500

    1.1

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function ModoOscuro(){
-    const color= document.body.style.backgroundColor
-    if (color==="f3e5d8" || color===""){
-        document.body.style.backgroundColor="black"
-    }else{
-        document.body.style.backgroundColor="#f3e5d8"
-    } 
-} 
+document.getElementById('colorButton').addEventListener('click', function() {
+    const currentColor = document.body.style.backgroundColor;
+    const comidas = document.querySelectorAll('.comidas');
+
+    if (currentColor === 'black') {
+      document.body.style.backgroundColor = '#f3e5d8';
+      comidas.forEach(eat => {
+        eat.style.backgroundColor = 'white';
+        eat.style.color = 'black';
+      })
+
+
+    } else {
+      document.body.style.backgroundColor = 'black';
+      comidas.forEach(eat => {
+        eat.style.backgroundColor = 'grey';
+        eat.style.color = 'white';
+      })
+
+
+    }
+  });
